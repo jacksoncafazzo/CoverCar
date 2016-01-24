a073b6311b7fd20a31b2c1f5f004430beb17e303
Jackson Cafazzo <doctorjuno@gmail.com>
2016-01-24 04:24:42 -0800

    add insurance user interface

--- js/scripts.js
+++ js/scripts.js
@@ -20,6 +20,29 @@ Contact.prototype.fullName = function() {
 }
 
 $(document).ready(function() {
+  $("#add-insurance").click(function() {
+    $("#new-addresses").append('<div class="new-address">' +
+      '<div class="form-group">' +
+        '<label for="new-company-name">Street</label>' +
+        '<input type="text" class="form-control new-company-name">' +
+      '</div>' +
+      '<div class="form-group">' +
+        '<label for="new-policy-number">City</label>' +
+        '<input type="text" class="form-control new-policy-number">' +
+      '</div>' +
+      '<div class="form-group">' +
+        '<label for="new-phone-number">State</label>' +
+        '<input type="text" class="form-control new-phone-number">' +
+      '</div>' +
+    '</div>');
+  });
+
+
+  var inputtedCompanyName = $("input#new-company-name").val();
+  var inputtedPolicyNumber = $("input#new-policy-number").val();
+  var inputtedAgentPhone = $("input#new-phone-number").val();
+
+
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
 
@@ -27,6 +50,14 @@ $(document).ready(function() {
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
     var newContact = new Contact(inputtedCabNumber, inputtedFirstName, inputtedLastName);
+
+    $(".new-insurance-info").each(function() {
+      var inputtedCompanyName = $(this).find("input#new-company-name").val();
+      var inputtedPolicyNumber = $(this).find("input#new-policy-number").val();
+      var inputtedAgentPhone = $(this).find("input#new-agent-phone").val();
+      var newInsuranceInfo = new insuranceInfo(inputtedCompanyName, inputtedPolicyNumber, inputtedAgentPhone)
+      newContact.insuranceInfo.push(newInsuranceInfo)
+    });
 
     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
